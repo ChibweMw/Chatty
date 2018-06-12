05f5bea0fd57aba8b26708cca3a19d4be7ef4381
Chibwe Mweene <caleidosymmetry@gmail.com>
2018-06-12 04:25:10 +0000

    Initial components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
+
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <body>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList />
+        <ChatBar />
+      </body>
     );
   }
 }
---
+++ src/MessageList.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import Message from './Message.jsx';
+import Notification from './Notification.jsx'
+
+class MessageList extends Component {
+  render() {
+    return (
+      <main className="messages">
+        <Message />
+        <Notification />
+      </main>
+    );
+  }
+}
+
+export default MessageList;
