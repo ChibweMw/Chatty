1932b36519ced9e3388c0727e563a79815970599
ChibweMw <caleidosymmetry@gmail.com>
2024-11-22 17:42:45 -0800

    Auto scroll feature in message list added.

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -13,18 +13,26 @@ class MessageList extends Component {
     this.scrollDiv = null;
   }
 
+  // Set the property to the selected component to be used as a scroll div
+  // The scroll div is for auto scroll reference
   setScroll = (el) => {
     if (!this.scrollDiv) this.scrollDiv = el;
   }
+
+  // Scroll the div into view once it is out of 'view'
   Scroll = () => {
     console.log(`>> =======================`)
-    if (this.scrollDiv) this.scrollDiv.scrollIntoView({ behavour: "smooth" });
+    if (this.scrollDiv) this.scrollDiv.scrollIntoView(true);
+    // if (this.scrollDiv) this.scrollDiv.scrollIntoView({ behavour: "smooth" });
   }
+  // From Docs: "By default, the element is aligned to the top (or bottom) edge of the scrollable ancestor. 
+  // To define a custom spacing, use 'scroll-margin-top' or 'scroll-margin-bottom'. 
+  // This is often useful when there's a fixed header on the page."
   render() {
     const availableMessages = this.props.message
     const numberOfMessages = availableMessages.length
     return (
-      <main className="messages" style={{marginBottom: 11 + 'vh'}}>
+      <main className="messages" style={{paddingBottom: 20 + 'px'}}>
         {availableMessages.map((userMessage) => {
           if (userMessage.type === "incomingNotification") {
             console.log("Message type is",userMessage.type)
@@ -34,7 +42,8 @@ class MessageList extends Component {
           }
         })}
         { this.Scroll() }
-        <div ref={ this.setScroll }></div>
+        <div ref={ this.setScroll } ></div>
+        {/* <div ref={ this.setScroll } style={{paddingTop: 20 + 'px'}} ></div> */}
       </main>
     );
   }
